db5156406876859c396fc7c2bab80cd253768c9f
yemref <emrefakiroglu@gmail.com>
2024-04-25 12:20:58 +0300

    Initial Commit

---
+++ BirthdayNoteTaker/BirthdayNoteTakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirthdayNoteTakerApp.swift
+//  BirthdayNoteTaker
+//
+//  Created by yunus emre fakiroğlu on 25.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct BirthdayNoteTakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
